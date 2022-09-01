0905b78f7d758ef37159dc4c753dca89161103b3
markebaird <33502889+markebaird@users.noreply.github.com>
2022-09-01 14:18:10 +1000

    Original files

---
+++ Bleach.m
@@ -0,0 +1,306 @@
+function dydt = bleach(t, y, inputs)
+
+% developed by Mark Baird and Julie Terp Jorgensen (2021).
+
+% Water column environmental conditions;
+  
+% Extract 'inputs' variables created in run_bleach
+DIN_w = inputs{1};
+DIP_w = inputs{2};
+temp_timeseries = inputs{3};
+POM = inputs{4};
+salinity = inputs{5};
+tau = inputs{6};
+rho = inputs{7};
+Peak = inputs{8};
+
+
+% Quick calculations
+Tanomaly = interp1(temp_timeseries(1,:),temp_timeseries(2,:),t); % Temperature anamoly at any time
+Ed =Peak*max(0,sin((t-0.25-floor(t-0.25))*2*pi)); % W m-2 % Light level at any time
+
+
+
+
+
+
+a_rub = min(1.0,max(0,(1-exp(-(2-Tanomaly)))/(1-exp(-2))));
+
+% Parameters required for conversion to spectrally-resolved:
+
+c = 2.998e8;                % Speed of light                                    (m s-1)
+h = 6.626e-34;             % Planck constant                                   (J s-1)
+AV = 6.02e23;               % Avagadro constant                                 (mol-1)
+
+  
+dydt(1:length(y)) = 0; % this sets every derivative to 0 so I can progressively add to them.
+% Table A.1 (Baird et al. 2018)
+
+% State variables:
+  
+CS_N = y(1); % Zooxanthellae biomass                         CS     (mg N m-2)
+RN   = y(2); % Reserves of nitrogen                          RN     (mg N m-2)
+RP   = y(3);  % Reserves of phosphorus                        RP     (mg P m-2)
+RC   = y(4);  % Reserves of carbon                            RC     (mg C m-2)
+CH_N   = y(5);  % Coral biomass                                 CH     (mg N m-2)
+Chl  = y(6); % Zooxanthellae chlorophyll a concentration     Chl    (mg m-2)
+Xp = y(7); % Zooxanthellae diadinoxanthin concentration    Xp     (mg m-2)
+Xh = y(8); % Zooxanthellae diatoxanthin concentration       Xh     (mg m-2)
+Qox = y(9);% Oxidised reaction centre concentration        Qox    (mg m-2)
+Qred = y(10);% Reduced reaction centre concentration         Qred   (mg m-2)
+Qin = y(11); % Inhibited reaction centre concentration       Qin    (mg m-2)
+ROS = y(12);% Reactive oxygen species concentration         [ROS]  (mg m-2]
+
+Qtot = Qox + Qin + Qred;
+
+% Constant values table A.7 in Baird et al. 2018:
+
+D_N = 17.5e-10;              % Molecular diffusivity of NO3                      (m2 s-1)
+D_P = 8.46e-10;              % Molecular diffusivity of NO3                      (m2 s-1)
+v = 1.05e-6;               % Kinematic viscosity of water                      (m2 s-1)
+
+% Conversion of W m-2 to mmol photon d-1 for light at 490 nm. 
+  
+W2mmol_490 = 8.359335857479461e-9*490*1000*86400; %  = (1e9 h c)-1 / Av
+
+% Parameter values table A.7 in Baird et al. 2018:
+
+mN = 4.514805e-14*16*1000*14.01;  % Nitrogen content of zooxanthellae cells           (mg N cell-1)
+rCS = 5e-6;                        % Radius of zooxanthellae cells                     (m)
+CSvol = 4*pi/3*rCS^3;    % volume of the cell (m3)
+uCH = 0.05;                     % Maximum growth rate of coral host                     (d-1)
+Spart = 3.0;                    % Rate coefficient of particle capture              (m d-1)
+uCS = 0.4;                      % Maximum growth rate of zooxanthellae              (d-1)
+zetaCH = 0.0; %0.01;                  % Quadratic mortality coefficient of polyps         (d-1 (g N m-2)-1)
+zetaCS = 0.0;                  % Linear mortality of zooxanthellae                 (d-1) 
+CHremin = 0.5;                   % Remineralised fraction of coral mortality 
+OCH = 2.0/1000;                      % Nitrogen-specific host area coefficient of polyps (m2 mg N-1)
+C2Chlmin = 20;                  % max Chl:C ratio at which synthesis stops.    
+
+% phi = 0.1                      % Fractional (of uCS) respiration rate
+
+Chlmax = 2.0*2.09e7 * ((1.0e18*CSvol)^-0.310);
+
+Xanth_tau = 72; % 1 per 20 minutes
+photon2rcii = 0.1e-6;
+ROSthreshold = 1.418e-14;
+photon2ros = 7000.0;
+chla2rcii = 0.002/893.49;
+CSmaxbleachrate = 1.0; % d-1
+
+% Schmidt number  = diffusivity momentum / diffusivity of nutrient ions
+
+Sc_N = v / D_N; % ratio between the kinematic viscosity of water and the molecular diffusivity of NO3
+Sc_P = v / D_P; % ratio between the kinematic viscosity of water and the molecular diffusivity of PO4
+
+% Mass transfer rate coefficient (height of water cleared of nutrient per unit of time by the watercoral 
+% exchange): 
+
+Sx_N = 2850 * (2 * tau / rho)^0.38 * Sc_N^-0.6;  % (m d-1)
+Sx_P = 2850 * (2 * tau / rho)^0.38 * Sc_P^-0.6; % (m d-1)
+
+Aeff = 1 - exp(-OCH * CH_N); % Aeff is the effective projected area fraction of the coral community (m2 m?2) 
+
+% Translocation (represents the one-way consumption of zooxanthellae organic matter produced through either 
+% zooxanthellae growth or mortality):
+
+% Derived calculations: 
+
+cells = CS_N / mN;
+  
+Nquota = RN / CS_N; % normalised reserves
+kN_mass = DIN_w * Sx_N * Aeff;
+Nuptake = kN_mass * (1.0 - Nquota) ; % Dissolved inorganic nitrogen (DIN)(mg N m-2 s-1) 
+
+Pquota = RP / CS_N / ((1/16) * (30.97/14.01));
+kP_mass = DIP_w * Sx_P * Aeff;
+Puptake = kP_mass * (1.0 - Pquota); % Dissolved inorganic phosphorus (DIP)(mg P m-2 s-1)
+
+% pigment synthesis - do calculations at 490 nm assuming it is PAR.
+
+cellXp = Xp/(cells*CSvol);    % mg pig m-3
+cellXh_= Xh/(cells*CSvol);
+cellChl = Chl/(cells*CSvol);
+
+yC =  cellXp * 0.06 + cellChl * 0.06; %   cellXp * bio->yC_diadinoxanthin[w] + cellChl * bio->yC_symbiodinium[w];
+
+% calculate the absorption cross-section (value between 0 and pi r^2
+
+temp = 2.0 * yC * rCS;
+aA = 0.0;
+if (temp > 3e-4)
+  aA = pi * rCS * rCS * (1.0 - 2.0 * (1.0 - (1.0 + temp) * exp(-temp)) / (temp * temp));
+end				  
+
+% calculate fraction benefit of increasing pigment concentration [number between 0 and 4/3.
+  
+temp = yC*rCS;
+
+Chlsynfactor = (1.0-exp(-2.0*temp)*(2.0*temp*temp+2.0*temp+1.0))/(temp*temp*temp);
+
+Iquota = RC / CS_N / ((106/16) * (12.01/14.01));
+									 
+dChldt_syn = Chlmax * uCS * min(1.33,max(0.0,Chlsynfactor * (1.0 - Iquota) * (1.0 - (Qin/Qtot))));
+
+if (CS_N * 5.6786 < C2Chlmin * Chl ) %  don't synthesise if can't fit in any more. */
+  dChldt_syn = 0.0;
+end
+if Ed == 0
+  dChldt_syn = 0.0;
+end
+dydt(6) = dydt(6) + dChldt_syn * CSvol * cells;
+dydt(7) = dydt(7) +  0.2448 * dChldt_syn * CSvol * cells ;
+
+% aA - m2 cell-1; cells ind./m2 
+								 
+Iuptake = Ed * aA * Qox/Qtot * a_rub * W2mmol_490 * (1.0 - Iquota) * cells; % mmol m-2 d-1
+
+symb_grow_IN = uCS * Nquota * Pquota * Iquota;
+
+CStoCHfrac = (cells * pi * rCS * rCS) / (2 * CH_N * OCH);
+
+% eq. 7 in Baird et al. 2018 where growth rate of zooxanthellae is given by the maximum growth rate, uCS, 
+% multiplied by the normalised reserves, Nquota, Pquota and Iquota. Iquota is the energy/carbon reserve. 
+
+% Calculations used in the derivatives:
+
+% host feeding
+
+if Ed > 0
+ Spart = 0.0;
+end
+    
+pot_grazing = Spart * Aeff* POM; % change units from mg to g.
+
+total_grazing = min([uCH*CH_N, pot_grazing]); % g N m-2 s-1
+
+polypmort = zetaCH * CH_N;
+
+symb_grow = min([uCS,symb_grow_IN + CHremin * polypmort * CH_N / CS_N]);
+
+mucus = max([0.0,symb_grow_IN * CS_N + CHremin * polypmort * CH_N - uCS * CS_N]);
+
+mucus_attNR = max([0.0,mucus - CHremin * polypmort * CH_N]);
+
+% translocate (+ve symbiont to host), including growth translocates, 
+%       mortality of symbionts and mortality of host (-ve). */
+
+translocate = symb_grow * CS_N * CStoCHfrac + zetaCS * CS_N;
+
+translocate_attNR = symb_grow * CS_N * CStoCHfrac;
+
+host_growth = total_grazing + translocate;
+
+%  Mucus production in g N 
+ 
+mucus2 = max(0.0,host_growth - uCH*CH_N);
+
+host_growth = host_growth - mucus2;
+
+mucus = mucus + mucus2;
+
+% check they are zero:
+									   
+translocate = 0;
+polypmort = 0;
+total_grazing = 0;
+host_growth = 0;
+									 
+% symbiont and host growth  
+
+dydt(1) = symb_grow * CS_N - translocate - polypmort * CS_N;
+dydt(5) = host_growth - polypmort * CH_N + total_grazing;
+
+% changes in internal reserves
+
+dydt(2) = dydt(2) + Nuptake - symb_grow_IN * CS_N - translocate_attNR * Nquota; 
+dydt(3) = dydt(3) + Puptake - (symb_grow_IN * CS_N + translocate_attNR * Pquota) * ((1/16) * (30.97/14.01)) ; % Redfield ratio 106C:16N:1P
+dydt(4) = dydt(4) + Iuptake - (symb_grow_IN * CS_N +  translocate_attNR * Iquota)* ((106/16) * (12.01/14.01)); % Redfield ratio 106C:16N:1P
+									 
+% xanothophyll switching
+
+xans = Xp+Xh;
+
+metric = Qin/Qtot;
+parabole = 1.0;
+if (metric > 0.5)
+  if (Xh >= Xp)
+    parabole = 1.0-4.0*(Xp/xans-0.5)^2;
+  end
+else									 
+  if (Xp >= Xh)
+    parabole = 1.0-4.0*(Xh/xans-0.5)^2;     
+  end
+end
+% factor of 8 = 2^3 so that metric-0.5 ranges between -1 and 1.
+
+swwitch = 8.0*(metric-0.5)^3 * Xanth_tau * parabole * xans;									 
+      
+dydt(7) = dydt(7) - swwitch;  %Xp
+dydt(8) = dydt(8) + swwitch;  %Xh
+
+% symbiont mortality
+
+dydt(1) = dydt(1) - zetaCS * y(1);
+dydt(2) = dydt(2) - zetaCS * y(2);
+dydt(3) = dydt(3) - zetaCS * y(3);
+dydt(4) = dydt(4) - zetaCS * y(4);
+dydt(6) = dydt(6) - zetaCS * y(6);
+dydt(7) = dydt(7) - zetaCS * y(7);
+dydt(8) = dydt(8) - zetaCS * y(8);
+dydt(9) = dydt(9) - zetaCS * y(9);
+dydt(10) = dydt(10) - zetaCS * y(10);
+dydt(11) = dydt(11) - zetaCS * y(11);
+dydt(12) = dydt(12) - zetaCS * y(12);
+
+% reaction centre dynamics
+
+% 268 might have time units in it, and need to be divided by 86400?
+									 
+Qi2Qox = 268.0 * photon2rcii * Qin * 86400; % this repairs 10 mol ph m-2 d-1 hitting 1 mmol of RCII.
+ARO = uCS * Nquota * Iquota * Pquota * ROS;  %  rate of detoxification.
+
+absorb = Ed * aA * W2mmol_490 * cells * photon2rcii; %  units now mmol rcii m-2 d-1 
+C_fix = absorb * (Qox/Qtot) * a_rub * (1.0 - Iquota);
+C_notfixed = absorb * (Qox/Qtot) - C_fix;									 
+
+dydt(9) = dydt(9) + dChldt_syn * CSvol * cells * chla2rcii;   %Ox								 
+dydt(9) = dydt(9) - C_notfixed + Qi2Qox;                      %Ox
+dydt(10) = dydt(10) + C_notfixed - absorb * (Qred / Qtot);    %Red
+dydt(11) = dydt(11) - Qi2Qox + absorb * (Qred / Qtot);        %In
+dydt(12) = dydt(12) - ARO + 0.5 * (Qin/Qtot) * absorb / photon2rcii / photon2ros / 86400;   %ROS
+
+% why do we have 0.5 in above equation? Only seems to work with /86400, which I don't understand
+									 
+% now need to expel:
+
+ROSpercell = y(12)/cells;
+
+expulsionrate = CSmaxbleachrate*min(1.0,max(0.0,(ROSpercell - ROSthreshold)/ROSthreshold));
+
+dydt(1) = dydt(1) - expulsionrate * y(1);
+dydt(2) = dydt(2) - expulsionrate * y(2);
+dydt(3) = dydt(3) - expulsionrate * y(3);
+dydt(4) = dydt(4) - expulsionrate * y(4);
+dydt(6) = dydt(6) - expulsionrate * y(6);
+dydt(7) = dydt(7) - expulsionrate * y(7);
+dydt(8) = dydt(8) - expulsionrate * y(8);
+dydt(9) = dydt(9) - expulsionrate * y(9);
+dydt(10) = dydt(10) - expulsionrate * y(10);
+dydt(11) = dydt(11) - expulsionrate * y(11);
+dydt(12) = dydt(12) - expulsionrate * y(12);
+%dydt(13) = Ed;
+									 
+dydt = dydt';
+% Need to turn off derivatives if CS_N is less than 1000 times the ODE tolerance.
+
+if (y(1) < 1e-5)
+ dydt = dydt*0;
+end
+
+
+
+
+
+
+
---
+++ Run_Bleach.m
@@ -0,0 +1,203 @@
+%% Run bleach model
+
+% developed by Mark Baird and Julie Terp Jorgensen (2021).
+
+% Description: 
+% This process contains a coral model with autotrophic and heterotrophic growth, zooxanthellae physiology, 
+% xanthophyll cycle, reaction centre dynamics and reative oxygen build-up.
+
+% Differences from coral bleaching model in eReefs 
+
+% 1. Downwelling irradiance is not spectrally-resolved.
+% 2. No temperature dependence of physiological rates (except hardwired through a_rub)
+% 3. Host dynamics not working yet.
+    
+close all 
+clear all
+
+tspan = [0 26];
+
+% initial conditions:
+
+CS_N0 = 0.4;
+CH_N0 = 1000;
+CS_Chl = CS_N0*5.6786/30/20; % 1/20th of maximum 
+
+y0(1) = CS_N0; 
+y0(2) = CS_N0*0.5;
+y0(3) = CS_N0*(1*((1/16)*(30.97/14.01)))*0.5;
+y0(4) = CS_N0*(((106/16)*(12.01/14.01)))*0.5;
+y0(5) = CH_N0;
+
+% The 15 entries where the first 4 are multiplied with CS_N0
+% units: y(1): mg N, y(2): dimensionless, y(3): dimensionless, y(4): dimensionless
+
+y0(6) = CS_Chl;
+y0(7) = CS_Chl*0.2448*0.33;
+y0(8) = CS_Chl*0.2448*0.67;
+
+chla2rcii = 0.002/893.49;
+
+y0(9) = CS_Chl * chla2rcii*0.05;
+y0(10) = CS_Chl * chla2rcii*0.1;
+y0(11) = CS_Chl * chla2rcii*0.85;
+y0(12) = 0.0;
+
+disp('Initial conditions: [CS_N Rn RP RC CH_N Chl Xp Xh Qox Qred Qin ROS]');
+
+y0
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Enter in inputs here (lukes code fixers) 
+
+DIN_w = 40.71428571; % (mg N m-3);
+DIP_w = 13.57142857*(1*((1/16)*(30.97/14.01))); % (mg P m-3)
+
+% temperature timeseries, 1st row is times (in days) 2nd is temperature
+% anamoly at those times (this can be altered to come from a file)
+temp_timeseries = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26;
+                    0 0 0.7 1.8 2.9 4 4 4 4 4 4 4 4 4 4 4 4 4 4 2.79 1.2 0 0 0 0 0 ]; 
+
+POM = 0.0; % Particulate organic matter.
+salinity = 35;
+tau = 0.3; % sheer stress at the bottom (N m-2) - values between 0-1
+rho = 1026; % density of the water (kg m-3)
+  
+Peak = 500; %Peak daily sunlight (W/m2)
+
+
+
+inputs = cell(10,1); % This must be the right number, if more variables are added
+inputs{1} = DIN_w;
+inputs{2} = DIP_w;
+inputs{3} = temp_timeseries;
+inputs{4} = POM;
+inputs{5} = salinity;
+inputs{6} = tau;
+inputs{7} = rho;
+inputs{8} = Peak;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+opts = odeset('RelTol',1e-8,'AbsTol',1e-8,'MaxStep',60/86400);
+
+[t,y] = ode45(@B_3_12, [tspan], y0, opts, inputs);
+
+% calculations for plotting:
+
+% absorption cross-section (value between 0 and pi r^2)
+
+rCS = 5e-6;mN = 4.514805e-14*16*1000*14.01; 
+cells = y(:,1) /mN;CSvol = 4*pi/3*rCS^3;
+C2Chlmin = 20;
+ROSthreshold = 1.418e-14;
+CSmaxbleachrate = 1.0; % d-1
+				  
+cellXp = y(:,7)./(cells*CSvol);    % mg pig m-3
+cellXh_= y(:,8)./(cells*CSvol);
+cellChl = y(:,6)./(cells*CSvol);
+				  
+absorb =  cellXp * 0.06 + cellChl * 0.06;
+temp = 2.0 * absorb * rCS;
+aA = pi * rCS * rCS * (1.0 - 2.0 * (1.0 - (1.0 + temp) .* exp(-temp)) ./ (temp .* temp));
+
+nChl2C = y(:,6)./y(:,1)/5.6786*C2Chlmin;
+
+ROSpercell = y(:,12)./cells;
+expel = CSmaxbleachrate*max(0.0,(ROSpercell - ROSthreshold)./ROSthreshold)*ROSthreshold;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Lukes edit to calculate the light same as is calculated in B_3_12
+Ed = 0;
+SunFactor = 1 - ShadeFactor;
+for i = 1:length(t)
+    time = t(i);
+    Light =Peak*max(0,sin((time-0.25-floor(time-0.25))*2*pi)); % W m-2
+        if (ShadeTimes(1) < time - floor(time)) && (time - floor(time) < ShadeTimes(2))
+            Light = Light*SunFactor;
+        end
+    Ed(i) = Light;
+end
+%Edmax = 50;
+plot (t,Ed)
+%Ed = Edmax*max(0,sin((t-0.25-floor(t-0.25))*2*pi));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% rctotal = sum(y(:,9:11),2);
+% 
+% 
+% figure(1)
+% subplot(231)
+% pp = plot(t,y(:,[1]), 'LineWidth', 2);
+% ylabel('Zooxanthallae Biomass (mg N m-2)','FontSize', 8);
+% xlim([0 26])
+% title ('24 h shade','FontSize',8)
+% %text(0.04,0.99,'A','Units', 'Normalized', 'VerticalAlignment', 'Top','FontSize',12)
+% %xticklabels({})
+% 
+% 
+% 
+% figure(1)
+% subplot(232)
+% pp = plot(t,y(:,[2:4 1]), 'LineWidth', 2); % y contains 15 variables 
+% legend(pp, 'RN', 'RP', 'RC','CS_N');
+% title('Symbiont biomass','FontSize', 16);
+% ylabel('Reserves','FontSize', 16);
+% xlabel('Time [days]', 'FontSize', 16);
+% 
+% subplot(233)
+% pp2 = plot(t, [y(:,2)./y(:,1) y(:,3)./y(:,1)/(((1/16)*(30.97/14.01))) y(:,4)./y(:,1)/(((106/16)*(12.01/14.01)))], 'LineWidth', 2);
+% lgnd = legend(pp2, 'N [mg N]', 'P [dimensionless]', 'C [dimensionless]', 'Location','northeast');
+% lgnd.FontSize = 6;
+% xlim([0 26])
+% ylim([0 1.2])
+% ylabel('Normalised reserves','FontSize', 8);
+% text(0.04,0.99,'D','Units', 'Normalized', 'VerticalAlignment', 'Top','FontSize',12)
+% xticklabels({})
+% 
+% 
+% 
+% subplot(234)
+% pp = plot(t,y(:,6:8), 'LineWidth', 2); % y contains 15 variables 
+% legend(pp, 'Chl', 'Xp','Xh');
+% title('Pigments', 'FontSize', 16);
+% ylabel('Pigments','FontSize', 16);
+% xlabel('Time [days]', 'FontSize', 16);
+% 
+% subplot(235)
+% pp = plot(t,y(:,9)./rctotal,t,y(:,10)./rctotal,t,y(:,11)./rctotal, 'LineWidth', 2); % y contains 15 variables 
+% ll = legend(pp, 'Qox', 'Qred', 'Qin');
+% title('Reaction centres', 'FontSize', 16);
+% ylabel('Fraction in each state','FontSize', 16);
+% xlabel('Time [days]', 'FontSize', 16);
+% 
+% subplot(236)
+% 					  
+pp = plot(t,ROSpercell,t,expel,t,ROSthreshold*ones(size(t)),'LineWidth',2); % y contains 15 variables 
+ylabel('ROS per cell, Expulsion','FontSize', 8);
+xlabel('Time (d)', 'FontSize', 8);
+lgd = legend(pp,'ROS','norm expulsion','Threshold', 'Location', 'northwest');
+lgd.FontSize = 6;
+set(gca,'ylim',[0 4*1.418e-14]);
+set(gca,'xlim',[0 26]);
+xlim = get(gca,'xlim')
+ylim = get(gca,'ylim');
+text(xlim(1) + 0.4*diff(xlim),ylim(1) + 0.53*diff(ylim),['Thres = ',num2str(ROSthreshold)]);
+text(0.04,0.99,'G','Units', 'Normalized', 'VerticalAlignment', 'Top','FontSize',12)
+
+% 			 
+% 					  
+% subplot(236) % opaqueness of the cell
+% pp = plot(t,aA/rCS/rCS/pi, t, nChl2C, t,Ed/Peak,'LineWidth', 2); % y contains 15 variables 
+% title('Opaqueness and C:Chl', 'FontSize', 16);
+% ylabel('[\alpha / PA],[norm. C:Chl,[norm. Ed]');
+% xlabel('Time [days]', 'FontSize', 16);
+% legend(pp,'opaq','norm Chl:C','norm Ed');
+% xlim = get(gca,'xlim');ylim = get(gca,'ylim');
+% text(xlim(1) + 0.2*diff(xlim),ylim(1) + 0.1*diff(ylim),['C:Chlmin = ',num2str(C2Chlmin)]);
+% 
+% set(gcf,'Position',[720 500 1005 598]);
+% eval(['print -dpng RB_3_12_',datestr(now,'dd-mmm-yyyy'),'.fig'])
+% 			  
